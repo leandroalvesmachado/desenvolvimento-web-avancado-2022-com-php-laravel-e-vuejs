785cc14b4c01cd27767563430b4cbe8f072eafb6
Leandro Machado <leandroalvesmachado@gmail.com>
2022-03-22 16:47:12 -0300

    salvando

--- Seção 17: APIs, WebServices e Rest/app_locadora_carros/app/Http/Controllers/CarroController.php
+++ Seção 17: APIs, WebServices e Rest/app_locadora_carros/app/Http/Controllers/CarroController.php
@@ -5,16 +5,41 @@ namespace App\Http\Controllers;
 use App\Models\Carro;
 use Illuminate\Http\Request;
 
+use App\Repositories\CarroRepository;
+
 class CarroController extends Controller
 {
+    public function __construct(Carro $carro)
+    {
+        $this->carro = $carro;
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $carroRepository = new CarroRepository($this->carro);
+
+        if ($request->has('atributos_modelos')) {
+            $atributosModelos = 'modelos:id,'.$request->atributos_modelos;
+            $carroRepository->selectAtributosRegistrosRelacionados($atributosModelos);
+        } else {
+            $carroRepository->selectAtributosRegistrosRelacionados('modelos');
+        }
+
+        // incompleto, melhorar
+        if ($request->has('filtro')) {
+            $carroRepository->filtro($request->filtro);
+        }
+
+        if ($request->has('atributos')) {
+            $carroRepository->selectAtributos($request->atributos);
+        }
+
+        return response()->json($carroRepository->getResultado(), 200);
     }
 
     /**
@@ -35,7 +60,15 @@ class CarroController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->carro->rules(), $this->carro->feedback());
+        $carro = $this->carro->create([
+            'modelo_id' => $request->modelo_id,
+            'placa' => $request->placa,
+            'disponivel' => $request->disponivel,
+            'km' => $request->km
+        ]);
+
+        return response()->json($carro, 201);
     }
 
     /**
--- Seção 17: APIs, WebServices e Rest/app_locadora_carros/app/Models/Carro.php
+++ Seção 17: APIs, WebServices e Rest/app_locadora_carros/app/Models/Carro.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Carro extends Model
 {
     use HasFactory;
+    protected $fillable = ['modelo_id', 'placa', 'disponivel', 'km'];
+
+    public function rules()
+    {
+        return [
+            'modelo_id' => 'required|exists:marcas,id',
+            'placa' => 'required',
+            'disponivel' => 'required',
+            'km' => 'required',
+        ];
+    }
 }
